2bc1b6adfb1b9111d70d2164cda980de5c965251
Graciela Alaniz <alanizcreative@gmail.com>
2020-11-29 17:53:19 -0500

    WIP
    Update front end dependencies and composer upgrade

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '720bc0d780a1480717f673a96142ea67470087db',
+    'name' => 'alanizcreative/wp-theme-formation',
+  ),
+  'versions' => 
+  array (
+    'alanizcreative/wp-theme-formation' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '720bc0d780a1480717f673a96142ea67470087db',
+    ),
+  ),
+);
